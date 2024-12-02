68b0ac471025b493a2581007a46878262ff9be8f
syyyj226 <126091213+syyyj226@users.noreply.github.com>
2024-12-03 00:23:34 +0900

    header.h: create

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <wiringPi.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <signal.h>
+
+#endif
