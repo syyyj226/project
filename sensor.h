43248a43f17befa6fe17474ed34359209837bbc2
syyyj226 <syyyj226@naver.com>
2024-12-06 10:11:23 +0900

    sensor.h: Update

--- sensor.h
+++ sensor.h
@@ -19,6 +19,14 @@
 #define POUT2 24
 #define PIN2 25
 
+#define POUT3 26
+#define PIN3 27
+
+#define POUT4 28
+#define PIN4 29
+
+#define obstacleDistance 5.0;
+
 static int initSensor()
 {
   // Enable GPIO pins
@@ -27,6 +35,25 @@ static int initSensor()
     printf("gpio export err\n");
     return (1);
   }
+
+  if (-1 == GPIOExport(POUT2) || -1 == GPIOExport(PIN2))
+  {
+    printf("gpio export err\n");
+    return (1);
+  }
+
+  if (-1 == GPIOExport(POUT3) || -1 == GPIOExport(PIN3))
+  {
+    printf("gpio export err\n");
+    return (1);
+  }
+
+  if (-1 == GPIOExport(POUT4) || -1 == GPIOExport(PIN4))
+  {
+    printf("gpio export err\n");
+    return (1);
+  }
+
   // wait for writing to export file
   usleep(100000);
 
@@ -37,27 +64,29 @@ static int initSensor()
     return (2);
   }
 
-  // init ultrawave trigger
-  GPIOWrite(POUT1, 0);
-
-  // Enable GPIO pins
-  if (-1 == GPIOExport(POUT2) || -1 == GPIOExport(PIN2))
-  {
-    printf("gpio export err\n");
-    return (1);
-  }
-  // wait for writing to export file
-  usleep(100000);
-
-  // Set GPIO directions
   if (-1 == GPIODirection(POUT2, OUT) || -1 == GPIODirection(PIN2, IN))
   {
     printf("gpio direction err\n");
     return (2);
   }
 
+  if (-1 == GPIODirection(POUT3, OUT) || -1 == GPIODirection(PIN3, IN))
+  {
+    printf("gpio direction err\n");
+    return (2);
+  }
+
+  if (-1 == GPIODirection(POUT4, OUT) || -1 == GPIODirection(PIN4, IN))
+  {
+    printf("gpio direction err\n");
+    return (2);
+  }
+
   // init ultrawave trigger
+  GPIOWrite(POUT1, 0);
   GPIOWrite(POUT2, 0);
+  GPIOWrite(POUT3, 0);
+  GPIOWrite(POUT4, 0);
 }
 
 static int disableSensor()
@@ -66,29 +95,34 @@ static int disableSensor()
   if (-1 == GPIOUnexport(POUT1) || -1 == GPIOUnexport(PIN1))
     return (4);
 
-  // Disable GPIO pins
   if (-1 == GPIOUnexport(POUT2) || -1 == GPIOUnexport(PIN2))
+    return (4);
+
+  if (-1 == GPIOUnexport(POUT3) || -1 == GPIOUnexport(PIN3))
+    return (4);
+
+  if (-1 == GPIOUnexport(POUT4) || -1 == GPIOUnexport(PIN4))
     return (4);
 }
 
 // 거리 return 하도록 수정
-double distance1()
+int forwardSensor() // 0 or 1 return
 {
   clock_t start_t, end_t;
   double time;
   double distance;
 
-  // init ultrawave trigger
+  // sensor 1
   GPIOWrite(POUT1, 0);
   usleep(10000);
-  // start
+
   if (-1 == GPIOWrite(POUT1, 1))
   {
     printf("gpio write/trigger err\n");
     return (3);
   }
 
-  // 1sec == 1000000ultra_sec, 1ms = 1000ultra_sec
+  usleep(10);
   GPIOWrite(POUT1, 0);
 
   while (GPIORead(PIN1) == 0)
@@ -98,33 +132,20 @@ double distance1()
     end_t = clock();
 
   time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
-  distance = time / 2 * 34000;
-
-  printf("Sensor 1 distance: %.2lf\n", distance);
-
-  /*if (distance < (double)criteria) // 거리 변경
+  if (time / 2 * 34000 < obstacleDistance)
     return 1;
-  else
-    return 0; */
-}
 
-double distance2()
-{
-  clock_t start_t, end_t;
-  double time;
-  double distance;
-
-  // init ultrawave trigger
-  GPIOWrite(POUT1, 0);
+  // sensor 2
+  GPIOWrite(POUT2, 0);
   usleep(10000);
-  // start
+
   if (-1 == GPIOWrite(POUT2, 1))
   {
     printf("gpio write/trigger err\n");
     return (3);
   }
 
-  // 1sec == 1000000ultra_sec, 1ms = 1000ultra_sec
+  usleep(10);
   GPIOWrite(POUT2, 0);
 
   while (GPIORead(PIN2) == 0)
@@ -134,14 +155,65 @@ double distance2()
     end_t = clock();
 
   time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
-  distance = time / 2 * 34000;
-
-  printf("Sensor 2 distance: %.2lf\n", distance);
-
-  /*if (distance < (double)criteria) // 거리 변경
+  if (time / 2 * 34000 < obstacleDistance)
     return 1;
   else
-    return 0;*/
+    return 0;
+}
+
+int forwardSensor() // 0 or 1 return
+{
+  clock_t start_t, end_t;
+  double time;
+  double distance;
+
+  // sensor 1
+  GPIOWrite(POUT3, 0);
+  usleep(10000);
+
+  if (-1 == GPIOWrite(POUT3, 1))
+  {
+    printf("gpio write/trigger err\n");
+    return (3);
+  }
+
+  usleep(10);
+  GPIOWrite(POUT3, 0);
+
+  while (GPIORead(PIN3) == 0)
+    start_t = clock();
+
+  while (GPIORead(PIN3) == 1)
+    end_t = clock();
+
+  time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
+  if (time / 2 * 34000 < obstacleDistance)
+    return 1;
+
+  // sensor 2
+  GPIOWrite(POUT4, 0);
+  usleep(10000);
+
+  if (-1 == GPIOWrite(POUT4, 1))
+  {
+    printf("gpio write/trigger err\n");
+    return (3);
+  }
+
+  usleep(10);
+  GPIOWrite(POUT4, 0);
+
+  while (GPIORead(PIN4) == 0)
+    start_t = clock();
+
+  while (GPIORead(PIN4) == 1)
+    end_t = clock();
+
+  time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
+  if (time / 2 * 34000 < obstacleDistance)
+    return 1;
+  else
+    return 0;
 }
 
 #endif
