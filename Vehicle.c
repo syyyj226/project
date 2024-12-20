565ddb9d653be5b752b44db7fa93109b390578cd
syyyj226 <syyyj226@naver.com>
2024-12-20 16:34:33 +0900

    update

--- Vehicle.c
+++ Vehicle.c
@@ -1,7 +1,3 @@
-#include <wiringPi.h>
-#include <pthread.h>
-#include <signal.h>
-
 #include "header.h"
 #include "GPIO.h"
 #include "sensor.h"
@@ -16,35 +12,31 @@ int main()
 
     // 필요한 변수
     char buffer[20] = {0};
-    char order[20] = {0};
-    char destination[5] = {0};
-    char previous, now; // 이전 목적지, 현재 목적지
-    int workState;      // 프로그램 업무 상태
-    int orderNum = 0;   // 처리한 주문 개수
+    char order[5] = {0};
+    char previous, now;       // 이전 목적지, 현재 목적지
+    char workState;           // 프로그램 업무 상태
+    int destination[3] = {0}; // 0: C, 1: B, 2: A
+    int orderNum = 0;         // 처리한 주문 개수
     int err = 0;
-
-    // init motor and sensor
-    initMotor();
-    initSensor();
 
     while (1)
     {
-        orderNum = 0;
         strftime(buffer, sizeof(buffer), "%a %b %d %H:%M:%S", tm_info);
-        printf("%s - 운행 준비 완료\n", buffer);
+        printf("%s - Vehicle is ready for operation \n", buffer);
 
         // 업무 유지 물어보기
-        printf("\n    운행 상태를 유지하시겠습니까?    \n");
+        printf("Do you want to maintain the operational status? \n");
         printf("            y / n            \n");
 
         // 유효한 입력을 받을 때까지 반복
         while (1)
         {
             scanf("%c", &workState);
+            getchar();
             if (workState == 'y' || workState == 'n')
                 break;
             else
-                printf("잘못된 입력입니다. 다시 입력해주세요.\n(y / n): ");
+                printf("Invalid input: Enter again \n(y / n): ");
         }
 
         // 업무 상태 처리
@@ -60,82 +52,77 @@ int main()
 
             // 주문 입력 문구 출력
             printf("------------------------------\n");
-            printf("  주문을 공백으로 구분해 입력해주세요  \n");
-            printf(" 형식: (주문 항목1) (주문 항목2)... \n");
+            printf("Enter the order without spaces\n");
             printf("------------------------------\n");
 
             // 주문 입력 및 처리
             fgets(order, sizeof(order), stdin);
-            order[strcspn(order, "\n")] = '\0';
 
-            if (strlen(order) > 8)
+            for (int i = 0; order[i] != '\n'; i++)
             {
-                printf("다시 입력하세요. \n");
-                err = 1;
-            }
-            else if (!err)
-            {
-                for (int i = 0; i < sizeof(order); i++)
+                if (order[i] != 'A' && order[i] != 'B' && order[i] != 'C')
                 {
-                    if (i % 2 == 0 && (order + i) != 0 && (*(order + i) != 'A' || *(order + i) != 'B' || *(order + i) != 'C'))
-                    {
-                        printf("다시 입력하세요. \n");
-                        err = 1;
-                    }
-                    if (i % 2 == 1 && *(order) + i != 0)
-                    {
-                        printf("다시 입력하세요. \n");
-                        err = 1;
-                    }
+                    printf("Invalid input: Enter again \n");
+                    err = 1;
+                    break;
                 }
+                else if (order[i] == 'A')
+                    destination[2] = 1;
+                else if (order[i] == 'B')
+                    destination[1] = 1;
+                else if (order[i] == 'C')
+                    destination[0] = 1;
             }
-            else
-            {
-                for (int i = 0, int j = 0; i < sizeof(order); i + 2)
-                {
-                    if (*(order + i) == 'A')
-                        destination[2] = 'A';
-                    else if (*(order + i) == 'B')
-                        destination[1] = 'B';
-                    else if (*(order + i) == 'C')
-                        destination[0] = 'C';
-                }
-            }
+            if (!err)
+                break;
         }
 
+        printf("%d %d %d\n", destination[0], destination[1], destination[2]);
+
+        previous = 'N';
+        now = 'N';
         // case 나누어 주행
-        do
+        while (1)
         {
-            for (int i = 0; i < 5; i++)
+            // 현재 목적지, 다음 목적지 설정
+            previous = now;
+            now = 'N';
+            for (int i = 0; i < 3; i++)
             {
-                // 현재 목적지, 다음 목적지 설정
-                previous = now;
-                now = 0;
-                if (*(destination + i) != 0)
+                if (destination[i] == 1)
                 {
-                    now = destination[i];
+                    if (i == 0)
+                        now = 'C';
+                    else if (i == 1)
+                        now = 'B';
+                    else if (i == 2)
+                        now = 'A';
+
                     destination[i] = 0;
+                    break;
                 }
-
-                if (previous == 0 && now != 0)
-                    move(now);
-                else if (previous != 0 && now != 0)
-                    moveTo(previous, now);
-                else if (previous != 0 && now == 0)
-                    returnOrigin(previous);
-
-                sleep(5);
             }
-        } while (now != 0);
+
+            if (previous == 'N' && now != 'N')
+                move(now);
+            else if (previous != 'N' && now != 'N')
+                moveTo(now);
+            else if (previous != 'N' && now == 'N')
+                moveRe();
+
+            sleep(1);
+            if (now == 'N')
+                break;
+
+            sleep(5);
+        }
 
         // 주행 완료 후 돌아가기
-        printf("%d 번째 주문 처리가 완료되었습니다.\n", ++orderNum);
+        printf("%d: The order has been processed\n", ++orderNum);
     }
 
-    disableSensor();
-
     strftime(buffer, sizeof(buffer), "%a %b %d %H:%M:%S", tm_info);
-    printf("%s - 금일 운행이 중단되었습니다.");
+    printf("%s - The vehicle operation has ended for today\n", buffer);
 
     return 0;
 }
